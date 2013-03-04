82890fb757bcf3aea3d2ae2345844ab985aa0b53
Gareth Conner <gareth@creativeconners.com>
2013-03-04 16:52:48 -0500

    Changed rendering to display fixed decimal values.

--- src/calc.js
+++ src/calc.js
@@ -1,30 +1,87 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Gareth
- * Date: 2/25/13
- * Time: 12:04 PM
- * To change this template use File | Settings | File Templates.
- */
-function CalcCtrl($scope) {
-    var num = 0.0;
+var module = angular.module("calc", []);
+
+module.directive('fixedNum', function() {
+    return {
+        restrict: 'EAC',
+        require: 'ngModel',
+        link: function(scope, element, attr, controller) {
+
+            renderview = function(viewValue){
+                var num;
+                num = viewValue * 1;
+                if (viewValue === '') {
+                    element.text('');
+                } else {
+                    var txt = num.toFixed(1);
+                    element.val(num.toFixed(1));
+                }
+            };
+
+            controller.$render = function() {
+                console.log("directive render");
+                value = controller.$viewValue;
+                element.val(value);
+                renderview(value);
+            };
+        }
+    };
+});
+
+module.controller("CalcCtrl", function ($scope) {
+//    $scope.qty = new QuantityVM(new Quantity(12));
     $scope.qty = new Quantity(12);
-    $scope.num = num;
-};
+});
 
-function Quantity(numOfPcs) {
-    var qty = numOfPcs;
-    var dozens = numOfPcs / 12;
+function QuantityVM(qty) {
+    this.model = qty;
 
-    Object.defineProperty(this, "qty",{
+    Object.defineProperty(this, "dozens", {
         get: function(){
-            return qty;
+            return this.model.dozens.toFixed(1);
         },
-        set: function(val){
-            val = parseFloat(val);
-            qty = val;
-            dozens = val / 12;
+        set: function(val) {
+            this.model.dozens = val;
         }
     });
+
+        Object.defineProperty(this, "qty",{
+        get: function(){
+            return this.model.qty.toFixed(1);
+        },
+        set: function(val){
+            this.model.setQty(val);
+        }
+    });
+
+    Object.defineProperty(this, "product", {
+        get: function() {
+            return this.dozens * this.qty;
+        }
+    });
+}
+
+function Quantity(numOfPcs) {
+    var dozens = numOfPcs / 12;
+
+//    this.qty = parseFloat(qty);
+    this.qty = numOfPcs;
+    dozens = numOfPcs / 12;
+
+    this.setQty = function(val){
+        this.qty = parseFloat(val);
+        dozens = this.qty / 12;
+    };
+
+//    Object.defineProperty(this, "qty",{
+//        get: function(){
+//            return qty;
+//        },
+//        set: function(val){
+//            val = parseFloat(val);
+//            qty = val;
+//            dozens = val / 12;
+//        }
+//    });
 
     Object.defineProperty(this, "dozens", {
        get: function(){
@@ -33,13 +90,13 @@ function Quantity(numOfPcs) {
        set: function(val) {
            val = parseFloat(val);
            dozens = val;
-           qty = dozens * 12;
+           this.qty = dozens * 12;
        }
     });
 
     Object.defineProperty(this, "product", {
         get: function() {
-            return dozens * qty;
+            return dozens * this.qty;
         }
     })
 };
--- src/horsepower.js
+++ src/horsepower.js
@@ -1,11 +1,136 @@
+var app = angular.module("horsepower", []);
+
+app.controller("HorsepowerCtrl", function ($scope, $filter) {
+    $scope.winch = new WinchVM(new Winch());
+    $scope.formattedReducerOutputTorque = $filter('number')($scope.winch.reducerOutputTorque, 0);
+});
+
+
 /**
- * Created with IntelliJ IDEA.
- * User: Gareth
- * Date: 2/25/13
- * Time: 10:54 AM
- * To change this template use File | Settings | File Templates.
+ * A ViewModel to format the winch properties for display.
+ * @param winch
+ * @constructor
  */
-function HorsepowerCtrl($scope, $filter) {
-    $scope.winch = new Winch();
-    $scope.winch.reducerOutputTorque = $filter('number')($scope.winch.reducerOutputTorque, 0);
+function WinchVM(winch) {
+    var model = winch;
+
+    Object.defineProperty(this, "horsepower", {
+        get: function() {
+            return parseFloat(model.horsepower.toFixed(1));
+        },
+        set: function(val) {
+            model.horsepower = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "motorRPM", {
+        get: function() {
+            return parseFloat(model.motorRPM.toFixed(1));
+        },
+        set: function(val) {
+            model.motorRPM = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "reducerRatio", {
+        get: function() {
+            return parseFloat(model.reducerRatio.toFixed(1));
+        },
+        set: function(val) {
+            model.reducerRatio = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "reducerRPM", {
+        get: function() {
+            return parseFloat(model.reducerRPM.toFixed(1));
+        },
+        set: function(val) {
+            model.reducerRPM = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "reducerEfficiency", {
+        get: function() {
+            return parseFloat(model.reducerEfficiency.toFixed(1));
+        },
+        set: function(val) {
+            model.reducerEfficiency = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "reducerOutputTorque",  {
+        get: function() {
+            return parseFloat(model.reducerOutputTorque.toFixed(1));
+        },
+        set: function(val) {
+            model.reducerOutputTorque = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "reducerSprocket", {
+        get: function() {
+            return parseFloat(model.reducerSprocket.toFixed(0));
+        },
+        set: function(val) {
+            model.reducerSprocket = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "drumSprocket", {
+        get: function() {
+            return parseFloat(model.drumSprocket.toFixed(0));
+        },
+        set: function(val) {
+            model.drumSprocket = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "drumDiameter", {
+        get: function() {
+            return parseFloat(model.drumDiameter.toFixed(1));
+        },
+        set: function(val) {
+            model.drumDiameter = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "drumTorque", {
+        get: function() {
+            return parseFloat(model.drumTorque.toFixed(1));
+        },
+        set: function(val) {
+            model.drumTorque = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "drumToReducerRatio", {
+        get: function() {
+            return parseFloat(model.drumToReducerRatio.toFixed(1));
+        }
+    });
+
+    Object.defineProperty(this, "pull", {
+        get: function() {
+            return parseFloat(model.pull.toFixed(1));
+        },
+        set: function(val) {
+            model.pull = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "cableSpeed", {
+        get: function() {
+            return parseFloat(model.cableSpeed.toFixed(1));
+        },
+        set: function(val) {
+            model.cableSpeed = parseFloat(val);
+        }
+    });
+
+    Object.defineProperty(this, "drumRPM", {
+        get: function() {
+            return parseFloat(model.drumRPM.toFixed(1));
+        }
+    });
 }
